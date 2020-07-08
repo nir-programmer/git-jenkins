400621cb460980a037aeb6465244ac40e18491ab
nir <niritzhak10@gmail.com>
2020-07-08 18:24:09 +0300

    Adding the getter and setter to the password field

--- src/org/nir/model/User.java
+++ src/org/nir/model/User.java
@@ -4,6 +4,18 @@ public class User
 {
 	private String name ;
 	private String password; 
+	
+	
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
 	public String getAddress()
 	{
 		return address;
