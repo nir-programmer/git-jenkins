ee59581222cc93ec90fa03604772198c02e16f6c
nir <niritzhak10@gmail.com>
2020-07-08 15:25:24 +0300

    Adding the getter and setter for name field

--- src/org/nir/model/User.java
+++ src/org/nir/model/User.java
@@ -2,5 +2,17 @@ package org.nir.model;
 
 public class User
 {
-	private String name ; 
+	private String name ;
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	} 
+	
+	
 }
